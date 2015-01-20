9b8d7175e6b142dbd84b84efc8ba1c4b354afc8c
Ristona <ristona@163.com>
2015-01-20 16:31:16 +0800

    add entity common code

--- entity/common/src/main/java/org/inovout/datastore/entity/common/DataStoreRegions.java
+++
@@ -1,6 +0,0 @@
-package org.inovout.datastore.entity.common;
-
-public class DataStoreRegions {
-	public static final String DATASTORE_CAHCE_REGION_ROOT_PATH = "/3cloud/datastore/";
-	public static final String PERSISTENCE = "persistence";
-}
